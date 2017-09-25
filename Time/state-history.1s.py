59e00b430af0c3b08990a5f950af848a44185cdc
Shayne Holmes <shayneh@qualtrics.com>
2017-09-25 08:21:14 -0700

    Change the extension to py

---
+++ Time/state-history.1s.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Use a string to represent the possible combinations in a trinary format: 0 is nothing, 1 is work (up), 2 is a break (down)
+
+def encodestatus( number ):
+
+status = ' ▝▗▘▀▚▖▞▄'
+
+print(status)
