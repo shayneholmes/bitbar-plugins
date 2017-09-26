4b420ba3ba875aa902f9c909f7d61b8fb8b1dfb3
Shayne Holmes <shayneh@qualtrics.com>
2017-09-26 12:25:11 -0700

    Place encoding logic in a function

--- Time/sparklines-png.1s.py
+++ Time/sparklines-png.1s.py
@@ -4,31 +4,24 @@ import base64
 import mmap
 import png
 
-mm = mmap.mmap(-1, 10000)
-
-# r=png.Reader(filename='../Downloads/icon.png')
-# r2=r.asRGBA()
-
-# pixels = list(r2[2])
-
 pixels = ['110010010011',
           '101011010100',
           '110010110101',
           '100010010011']
 
-lfunc = lambda k: 0
-afunc = lambda k: 255 if k == '1' else 0
+pixels = [[int(x) for x in row] for row in pixels]
 
-pixels = [[f(x) for x in row for f in (lfunc, afunc)] for row in pixels]
+def encodePngFromPixels( pixels ):
+    lfunc = lambda k: 0
+    afunc = lambda k: 255 if k > 0 else 0
+    pixels = [[f(x) for x in row for f in (lfunc, afunc)] for row in pixels]
+    mm = mmap.mmap(-1, 10000)
+    png.from_array(pixels, 'LA').save(mm)
+    size = mm.tell() + 1
+    mm.seek(0)
+    imgData = base64.b64encode(mm.read(size))
+    return imgData
 
-png.from_array(pixels, 'LA').save(mm)
-
-size = mm.tell() + 1
-
-mm.seek(0)
-
-img = base64.b64encode(mm.read(size))
-
-print("Test | templateImage=" + img)
+print("Test | templateImage=" + encodePngFromPixels(pixels))
 print("---")
 print(pixels)
