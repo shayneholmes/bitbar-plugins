87c81fd68cff525b9af816378f3da32a6cfd4d71
Shayne Holmes <shayneh@qualtrics.com>
2018-03-01 07:40:47 -0800

    Remove dependencies for better runtime
    Reduces time from 250ms to 90ms, mostly by removing numpy. Also remove
    the sprite conversion, since using files is faster.

--- Time/graphical-timer.1s.py
+++ Time/graphical-timer.1s.py
@@ -5,12 +5,10 @@
 # - Implement custom timers for shorter tasks
 # - Add an out-of-band timer that is separate from the main workflow one
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 import base64
 from datetime import datetime
 from io import BytesIO
-import itertools
-import numpy as np
 import os
 import sys
 import time
@@ -23,19 +21,6 @@ statusinfo = {
             'label': '',
             'duration': 0,
             'spritefile': 'disabled.png',
-            'sprite': [
-                '00000000000',
-                '00000000000',
-                '00000000000',
-                '00000000000',
-                '00000000000',
-                '00011111000',
-                '00000000000',
-                '00000000000',
-                '00000000000',
-                '00000000000',
-                '00000000000',
-                ]
             },
         1: {
             'name': 'work',
@@ -43,19 +28,6 @@ statusinfo = {
             'duration': 15*durationmultiplier,
             'completionsound': 'glass',
             'spritefile': 'work.png',
-            'sprite': [
-                '00111111100',
-                '01000000010',
-                '10010001001',
-                '10010001001',
-                '10010001001',
-                '10010101001',
-                '10010101001',
-                '10010101001',
-                '10001010001',
-                '01000000010',
-                '00111111100',
-                ]
             },
         2: {
             'name': 'break',
@@ -63,19 +35,6 @@ statusinfo = {
             'duration': 5*durationmultiplier,
             'completionsound': 'glass',
             'spritefile': 'break.png',
-            'sprite': [
-                '00111111100',
-                '01000000010',
-                '10011110001',
-                '10010001001',
-                '10010001001',
-                '10011110001',
-                '10010001001',
-                '10010001001',
-                '10011110001',
-                '01000000010',
-                '00111111100',
-                ]
             },
         }
         
@@ -124,32 +83,7 @@ def formattime(secs):
 exepath = os.path.realpath(os.path.dirname(sys.argv[0]))
 
 def getsprite():
-    # return Image.open(exepath + '/graphical-timer/' + getfield('spritefile'))
-    packed = getfield('sprite')
-    pixels = [[int(x) for x in row] for row in packed]
-    w,h = len(packed[0]),len(packed)
-    return Image.frombytes(mode='1', size=(w,h), data=np.packbits(pixels, axis=1))
-    return Image.fromarray(np.asarray(pixels), "L")
-
-def expandtosize( pixels, w, h ):
-    currenth = len(pixels)
-    currentw = len(pixels[0])
-    newh = max(h, currenth)
-    neww = max(w, currentw)
-    paddingtop = (newh - currenth) // 2
-    paddingbottom = (newh - currenth - paddingtop)
-    paddingleft = (neww - currentw) // 2
-    paddingright = (neww - currentw - paddingleft)
-    paddedpixels = []
-    paddedpixels += itertools.repeat(itertools.repeat(0,neww), paddingtop)
-    for i in range(currenth):
-        row = []
-        row += itertools.repeat(0, paddingleft)
-        row += pixels[i]
-        row += itertools.repeat(0, paddingright)
-        paddedpixels.append(row)
-    paddedpixels += itertools.repeat(itertools.repeat(0,neww), paddingbottom)
-    return paddedpixels
+    return Image.open(exepath + '/graphical-timer/' + getfield('spritefile'))
 
 def base64encodeImage( image ):
     output = BytesIO()
@@ -256,23 +190,16 @@ def drawProgressBar(draw, percent):
         x2 -= lw+pad
         draw.rectangle([(x1,y1+lw+pad),(int(x1 + (x2 - x1) * percent),y2-(lw+pad))],fill=color)
 
-def drawLabel(draw):
-    label = getfield('label')
-    font = ImageFont.truetype("/System/Library/Fonts/HelveticaNeueDeskInterface.ttc", size=24)
-    draw.text((128,0), label, font=font, fill=(0,255))
-
 height = 15 # sprite is square, so width should be the same
-sprite_height = 11
 
 padding = 2
-barheight = 13
+barheight = 15
 barwidth = 64
 width = height + padding + barwidth
 im = Image.new("LA", (width, height))
 draw = ImageDraw.Draw(im)
 drawProgressBar(draw, percent)
-# drawLabel(draw)
-draw.bitmap((0,(height-sprite_height)//2), getsprite(), fill=(0,255))
+draw.bitmap((0,0), getsprite(), fill=(0,255))
 del draw
 
 print("| templateImage=" + base64encodeImage(im))
